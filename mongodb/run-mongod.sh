07b2242fb56c59dfd3cf424b6a30a5f6f881515c
Siddharth Goel <siddharth98391@gmail.com>
2014-10-08 15:13:14 +0800

    Added some scripts
    Some scripts still remaining
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

---
+++ mongodb/run-mongod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=`id -u -n`
+HOST=`hostname`
+DIR="/temp/mongodb-$USER-$HOST"
+LOGDIR="$HOME/mongodb-logs/"
+
+#Delete the directory if it is existing
+rm -rf $DIR
+
+#Create new directory for mongodb data
+mkdir $DIR
+
+mkdir -p $LOGDIR
+
+export PATH=$PATH:$1:
+
+#$2 is the port on which we want to run mongod
+#nohup will run the mongod in background and store logs in $LOGDIR
+nohup mongod --dbpath $DIR --port $2 > "$LOGDIR/mongod-$HOST.log"
---
+++ mongodb/run-mongos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOST=`hostname`
+LOGDIR="$HOME/mongodb-logs/"
+
+#Delete the directory if it is existing
+rm -rf $DIR
+
+#Creates directory for logging if it does not exists
+mkdir -p $LOGDIR
+
+export PATH=$PATH:$1:
+
+#$2 is the port on which we want to run mongod are running while on
+#$3 mongos will run nohup will run the mongod in background and 
+#store logs in $LOGDIR
+nohup mongos --configdb compg4:$2,compg6:$2,compg7:$2 --port $3 > "$LOGDIR/mongos-$HOST.log"
