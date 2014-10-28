b5dcde3d11980b1602c15a022dad4a12545c5561
Siddharth Goel <siddharth98391@gmail.com>
2014-10-28 21:59:49 +0800

    Added code to generate graph for other scenarios
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- mongodb/graph/generate-graph.py
+++ mongodb/graph/generate-graph.py
@@ -5,7 +5,11 @@ import datetime
 import linecache
 from matplotlib import pyplot
 
-logdir = '$HOME/mongodb-logs'
+#Put the path of the directory where all the logs are stored
+logdir = '/Users/siddharthgoel/code/misc/run/'
+
+#All the different colors for the graph
+colors = ['bo-', 'go-', 'ro-', 'co-', 'mo-', 'yo-', 'ko-', 'bo--', 'go--', 'ro--', 'co--', 'mo--', 'yo--', 'ko--' ]
 
 class Stats:
 	def __init__(self):
@@ -13,6 +17,8 @@ class Stats:
 		self.expThr = 0.0	
 		self.runtime = 0.0
 		self.achThr = 0.0
+		self.avgUpdateLat = 0.0
+		self.achReadLat = 0.0
 
 
 def parseLastValue(string):
@@ -57,7 +63,7 @@ def save(path, ext='png', close=True, verbose=True):
 
 
 def achThrExpThrGraph(statsList):
-	colors = ['bo-', 'go-', 'ro-', 'co-', 'mo-', 'yo-', 'ko-', 'bo--', 'go--', 'ro--', 'co--', 'mo--', 'yo--', 'ko--' ]
+	global colors
 	threads = list(set([stats.threads for stats in statsList ]))
 	threads.sort()
 	numOfThreads = len(threads)
@@ -66,6 +72,7 @@ def achThrExpThrGraph(statsList):
 	numOfThr = len(expThr)
 	achThr = [stats.achThr for stats in statsList ]
 
+	pyplot.figure(1)
 	for i in range(numOfThreads):
 		pyplot.plot(expThr,achThr[(i*numOfThr):((i+1)*numOfThr) ],colors[i],label='Total Threads='+str(threads[i]))
 
@@ -79,6 +86,53 @@ def achThrExpThrGraph(statsList):
 	save("Mongodb-throughput-graph-"+str(st), ext="png", close=False, verbose=True)
 	#pyplot.show()
 
+def avgReadExpThrGraph(statsList):
+	global colors
+	threads = list(set([stats.threads for stats in statsList ]))
+	threads.sort()
+	numOfThreads = len(threads)
+	expThr = list(set([stats.expThr for stats in statsList ]))
+	expThr.sort()
+	numOfThr = len(expThr)
+	avgReadLat = [stats.avgReadLat for stats in statsList ]
+
+	pyplot.figure(2)
+	for i in range(numOfThreads):
+		pyplot.plot(expThr,avgReadLat[(i*numOfThr):((i+1)*numOfThr) ],colors[i],label='Total Threads='+str(threads[i]))
+
+	pyplot.grid(axis='both')
+	pyplot.xlabel('Expected Throughput (operations/second)')
+	pyplot.ylabel('Average Read Latency (milliseconds)')
+	pyplot.axis([0, 1.1 * max(expThr) , 0, 15000.0 ])
+	pyplot.title('Plot of Average Read Latency against Target Throughput in mongodb with various threads running about 250000 read operations.')
+	pyplot.legend(loc=2)
+	st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+	save("Mongodb-read-latency-graph-"+str(st), ext="png", close=False, verbose=True)
+	#pyplot.show()
+
+def avgUpdateExpThrGraph(statsList):
+	global colors
+	threads = list(set([stats.threads for stats in statsList ]))
+	threads.sort()
+	numOfThreads = len(threads)
+	expThr = list(set([stats.expThr for stats in statsList ]))
+	expThr.sort()
+	numOfThr = len(expThr)
+	avgUpdateLat = [stats.avgUpdateLat for stats in statsList ]
+
+	pyplot.figure(3)
+	for i in range(numOfThreads):
+		pyplot.plot(expThr,avgUpdateLat[(i*numOfThr):((i+1)*numOfThr) ],colors[i],label='Total Threads='+str(threads[i]))
+
+	pyplot.grid(axis='both')
+	pyplot.xlabel('Expected Throughput (operations/second)')
+	pyplot.ylabel('Average Update Latency (milliseconds)')
+	pyplot.axis([0, 1.1 * max(expThr) , 0, 20000.0 ])
+	pyplot.title('Plot of Average Update Latency against Target Throughput in mongodb with various threads running about 250000 update operations')
+	pyplot.legend(loc=2)
+	st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+	save("Mongodb-update-latency-graph-"+str(st), ext="png", close=False, verbose=True)
+	#pyplot.show()
 
 def main():
 	global logdir
@@ -96,12 +150,16 @@ def main():
 			expectedThroughput = matchObj.group(2)
 			runtime = parseLastValue( linecache.getline(filePath, 5) )
 			achievedThroughput = parseLastValue( linecache.getline(filePath, 6) )
+			averageUpdateLatency = parseLastValue( linecache.getline(filePath, 8) )
+			averageReadLatency = parseLastValue( linecache.getline(filePath, 1016) )
 
 			stats = Stats()
 			stats.threads = int(threads)
 			stats.expThr = int(expectedThroughput)
 			stats.runtime = float(runtime)
 			stats.achThr = float(achievedThroughput)
+			stats.avgUpdateLat = float(averageUpdateLatency)
+			stats.avgReadLat = float(averageReadLatency)
 			statsList.append(stats)
 			#output += threads + "\t\t" + expectedThroughput + "\t\t\t" + runtime + "\t\t\t" + achievedThroughput + "\n"
 
@@ -109,6 +167,8 @@ def main():
 	display(statsList)
 	print len(statsList)
 	achThrExpThrGraph(statsList)
+	avgReadExpThrGraph(statsList)
+	avgUpdateExpThrGraph(statsList)
 	#print output
 
 
